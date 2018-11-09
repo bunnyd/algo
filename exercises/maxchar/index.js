04eaebbf8f0ddee2c0f3031c07e8fe9db464f606
Nancy Do <nancydo7@gmail.com>
2018-11-09 11:28:46 -0600

    5 problems

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,44 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// function maxChar(str) {
+//   const chars = {};
 
+//   for (let char of str) {
+//     if (!chars[char]) {
+//       return (chars[char] = 1);
+//     } else {
+//       return chars[char]++;
+//     }
+//   }
+// }
+
+// function maxChar(str) {
+//   const chars = {};
+
+//   for (let char of str) {
+//     chars[char] = chars[char] + 1 || 1;
+//   }
+// }
+
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 module.exports = maxChar;
--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,29 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  return str.split("").every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
+}
 
 module.exports = palindrome;
+
+// function palindrome(str) {
+//   let reversed = "";
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
+//   if (str === reversed) {
+//     return true;
+//   }
+//   return false;
+// }
+
+// function palindrome(str) {
+//   const reversed = str.split("").reverse().join("")
+//   if (str === reversed) {
+//     return true;
+//   }
+//   return false;
+// }
